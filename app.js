a63e1283e7cb4a1d030b0184366ee4a52bf6e33b
Alucard2169 <ashish10deepanshu@gmail.com>
2022-05-20 14:05:19 +0530

    update v2

--- app.js
+++ app.js
@@ -1,56 +1,134 @@
-const container = document.querySelector('.container');
-const button = document.createElement('button');
-let size;
+let root = document.querySelector(':root');
+const gridButtons = document.querySelectorAll('.size');
+const custom = document.getElementById('custom');
+const submitBtn = document.querySelector(".submit");
+const clearBtn = document.querySelector('.clear');
+const randomBtn = document.querySelector('.random');
+const canvas = document.querySelector('.canvas'); 
+const colorBtn = document.querySelector('.colorBtn');
+const colorPick = document.querySelector("#color");
+const eraserBtn = document.querySelector('.eraser');
+const colorInput = document.querySelector('#color');
 
-button.classList.add('clear')
-button.innerText = 'CLEAR'
-document.body.appendChild(button)
+//default values
+colorPick.value = "#000000";
+custom.value = '';
+let currentMode = 'color'
+let currentColor = 'black'
 
-button.addEventListener('click', function (){
-    container.innerHTML = '';
-    size = prompt('what size do you want your grid to be: ');
-    checkSize(size);
-})
 
-let mouse_down = false;
+// check if mouse is clicked or not
+let mouseDown = false;
 document.body.onmousedown = function () {
-    mouse_down = true;
+    mouseDown = true;
 }
 document.body.onmouseup = function () {
-    mouse_down = false;
+    mouseDown = false;
 }
-function canvas(size) {
-    container.style.setProperty('--grid-column', size);
-    container.style.setProperty('--grid-row', size);
 
-    for (let i = 0; i < size*size; i++) {
-        const cell = document.createElement('div');
-        cell.addEventListener('mouseover', changeColor);
-        cell.addEventListener('mousedown', changeColor);
-        container.appendChild(cell).className = 'grid-items';
+
+
+
+// coloring mode function
+function setCurrentColor(newColor) {
+    currentColor = newColor;
+}
+
+// current color function
+function setCurrentMode(newMode) {
+    activeMode(newMode)
+    currentMode = newMode;
+}
+
+
+colorPick.onchange = (e) => setCurrentColor(e.target.value)
+
+colorBtn.addEventListener('click', () => setCurrentMode("color"));
+randomBtn.addEventListener('click', () => setCurrentMode("rainbow"));
+eraserBtn.addEventListener('click', () => setCurrentMode('erase'));
+
+// main coloring function
+function colorPicker(e) {
+  if (e.type === 'mouseover' && !mouseDown) return
+  if (currentMode === 'rainbow') {
+    const randomR = Math.floor(Math.random() * 256)
+    const randomG = Math.floor(Math.random() * 256)
+    const randomB = Math.floor(Math.random() * 256)
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+  } else if (currentMode === 'color') {
+    e.target.style.backgroundColor = currentColor
+  } else if (currentMode === 'erase') {
+      e.target.style.backgroundColor = '#ffffff';
     }
-}
-canvas(16)
-
-
-function changeColor(e) {
     
-    if(mouse_down){
-        let color1 = Math.floor(Math.random() * 256);
-        let color2 = Math.floor(Math.random() * 256);
-        let color3 = Math.floor(Math.random() * 256);
+}
 
-        e.target.style.backgroundColor = `rgb(${color1},${color2},${color3})`
+
+const grid = (value) => {
+     root.style.setProperty('--grid-col',value);
+    root.style.setProperty('--grid-row', value);
+    
+    for (let i = 0; i < value * value; i++){
+        const cell = document.createElement('div');
+        cell.addEventListener('mouseover',colorPicker);
+        cell.addEventListener('mousedown',colorPicker);
+        canvas.appendChild(cell);
     }
 }
 
-function checkSize(e) {
-    if (e > 100) {
-        alert("size can't be greater than 100");
-        canvas(16)
+// starter grid
+grid(16);
+
+// button to make 32x, 64x grid
+gridButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        grid(0)
+        canvas.innerHTML = '';
+       grid(button.value)
+    })
+})
+
+// make custom grid
+submitBtn.addEventListener('click', () => {
+    if (custom.value <= 0) {
+        alert("Invalid value, can only be number greater than 1");
+        custom.value = '';
+        return
+    }
+    else if (custom.value > 100) {
+        alert(`can't be greater than 100`);
+        custom.value = '';
+        return;
     }
     else {
-        canvas(e)
+        canvas.innerHTML = '';
+        grid(custom.value)
+        custom.value = ''
     }
-}
+})
 
+// clear canvas
+clearBtn.addEventListener('click', () => {
+    grid(0)
+    canvas.innerHTML = ''
+})
+
+
+const activeMode = (but) => {
+    if (but == 'rainbow') {
+        randomBtn.classList.add("active");
+        colorBtn.classList.remove("active");
+        eraserBtn.classList.remove("active")
+    }
+    else if(but == 'color'){
+        randomBtn.classList.remove("active");
+        colorBtn.classList.add("active");
+        eraserBtn.classList.remove("active")
+    }
+    else if (but === 'erase') {
+        randomBtn.classList.remove("active");
+        colorBtn.classList.remove("active");
+        eraserBtn.classList.add("active")
+
+    }
+} 
